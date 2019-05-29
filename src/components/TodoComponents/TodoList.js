fedd59f882cd6e1c217817d24f1961ab185a1a18
Hoxtygen <hoxtygen@live.com>
2019-05-29 12:29:58 +0100

    Display todos on the main page
    Here, the todos are made to be displayed on the main page. The list of todos in the Todo component are added to the TodoList component and looped over to be displayed one after the other, and afterwards, the Todo component is imported to the App component.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React, { Component } from 'react'
+
+export default class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                {
+                   // console.log(this.props.todos)
+                    this.props.todos.map(todoItem => {
+                        return <li key = {todoItem.id}>{todoItem.todoName}</li>
+
+                    })
+                }
+            </div>
+        )
+    }
+}
